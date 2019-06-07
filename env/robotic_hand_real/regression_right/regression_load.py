d50a7c065196db47317653305cc6164e627d3d93
chrisjtan <hust.jtt@gmail.com>
2019-06-07 13:45:00 -0700

    Add state 4 action 6 data for real hand

---
+++ env/robotic_hand_real/regression_right/regression_load.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pickle
+from common.BNN import BNN
+
+with open('../../../data/robotic_hand_real/t42_cyl45_right_data_discrete_v0_d4_m1.obj', 'rb') as pickle_file:
+    D, state_dim, action_dim, _, _ = pickle.load(pickle_file, encoding='latin1')
+
+DATA = D
+
+x_data = DATA[:, :10]
+y_data = DATA[:, 12:14] - DATA[:, 2:4]
+
+if __name__ == "__main__":
+    neural_network = BNN(nn_type='0')
+    neural_network.add_dataset(x_data, y_data, held_out_percentage=0.1)
+    neural_network.build_neural_net()
+    save_path = '../../../save_model/robotic_hand_real_s4_a6/pos'
+    neural_network.train(save_path=save_path, normalization=True, normalization_type='z_score', decay='True')
